1dc1e8a2d60fb68bc817b89ff1a016b91550bab2
RamonJales <ramonjales123@gmail.com>
2024-08-17 18:40:56 -0300

    tests

---
+++ src/test_rag.py
@@ -0,0 +1,41 @@
+from query_data import query_rag
+from langchain_community.llms.ollama import Ollama
+
+EVAL_PROMPT = """
+Expected Response: {expected_response}
+Actual Response: {actual_response}
+---
+(Answer with 'true' or 'false') Does the actual response match the expected response? 
+"""
+
+def test_ticket_to_ride_rules():
+    assert query_and_validate(
+        question="in talent versus luck study how many percents of people had a neutral life, without lucky or unlucky events? (Answer with the number only)",
+        expected_response="16%",
+    )
+
+
+def query_and_validate(question: str, expected_response: str):
+    response_text = query_rag(question)
+    prompt = EVAL_PROMPT.format(
+        expected_response=expected_response, actual_response=response_text
+    )
+
+    model = Ollama(model="mistral")
+    evaluation_results_str = model.invoke(prompt)
+    evaluation_results_str_cleaned = evaluation_results_str.strip().lower()
+
+    print(prompt)
+
+    if "true" in evaluation_results_str_cleaned:
+        # Print response in Green if it is correct.
+        print("\033[92m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return True
+    elif "false" in evaluation_results_str_cleaned:
+        # Print response in Red if it is incorrect.
+        print("\033[91m" + f"Response: {evaluation_results_str_cleaned}" + "\033[0m")
+        return False
+    else:
+        raise ValueError(
+            f"Invalid evaluation result. Cannot determine if 'true' or 'false'."
+        )
